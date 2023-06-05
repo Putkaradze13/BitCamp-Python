75ee486f46ec6b02122e340b425a9ec5f1208d87
Tengiz Putkaradze <phutkaradze.13@gmail.com>
2023-06-05 13:22:20 +0400

    adding task10 meal time

---
+++ meal/meal.py
@@ -0,0 +1,28 @@
+
+
+def main():
+    user_time = input('What time is it? ')
+    decimal_time = convert(user_time)
+    meal(decimal_time)
+
+
+def convert(t):
+    hours, minutes = t.split(':')
+    hours = float(hours)
+    minutes = float(minutes)
+    time = hours + minutes/60
+
+    return time
+
+
+def meal(dtime):
+    dtime = float(dtime)
+    if 7 <= dtime <= 8:
+        print('breakfast time')
+    elif 12 <= dtime <= 13:
+        print('lunch time')
+    elif 18 <= dtime <= 19:
+        print('dinner time')
+
+if __name__ == "__main__":
+    main()
