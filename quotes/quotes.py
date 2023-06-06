22b1897da891283f0186788f0769b092feb604ec
Tengiz Putkaradze <phutkaradze.13@gmail.com>
2023-06-06 09:46:38 +0400

    adding new task11

---
+++ quotes/quotes.py
@@ -0,0 +1,6 @@
+# App prompts user for quotes and then asks who's author and prints autor and quotes together.
+
+quotes = input('what is the quotes? ')
+autor = input('Who said it? ')
+
+print(f'{autor} says, \'{quotes}\'')
