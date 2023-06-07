5fccb9a6b2a4aed1244b0dd2938b3f5473dab9e6
Tengiz Putkaradze <phutkaradze.13@gmail.com>
2023-06-07 22:08:44 +0400

    uploading new task14 coke machine

---
+++ coke/coke.py
@@ -0,0 +1,20 @@
+# prompts the user to insert a coin, one at a time,
+# each time informing the user of the amount due.
+# Once the user has inputted at least 50 cents,
+# output how many cents in change the user is owed. 
+
+coins = [5, 10, 25]
+cost = 50
+i = 0
+
+while i < cost:
+    coin = int(input('Insert Coin: '))
+    if coin not in coins:
+        print(f'Amount Due: {cost - i}')
+    else:
+        i += coin
+        if i < cost:
+            print(f'Amount Due: {cost - i}')
+        else:
+            print(f'Change Owed: {i - cost}')
+            
